11b54e2f008034cdd21854bf68ecef347d10b5db
Traxmaxx <320272+Traxmaxx@users.noreply.github.com>
2025-06-27 13:49:50 +0200

    feat: add basic OLED display configuration

--- keymaps/traxmaxx/keymap.c
+++ keymaps/traxmaxx/keymap.c
@@ -70,6 +70,66 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif
 
 /**
+ * @brief Default OLED display configuration
+ *
+ * This provides a basic OLED display configuration
+ */
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
+    return rotation;
+}
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
+    oled_write_P(qmk_logo, false);
+}
+
+void print_status_narrow(void) {
+    // oled_write_P(PSTR("\n\n"), false);
+    oled_write_P(PSTR("LAYER"), false);
+    oled_write_P(PSTR("\n\n"), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_ln_P(PSTR("Base\n"), false);
+            break;
+        case _LAYER1:
+            oled_write_ln_P(PSTR("Layer 1\n"), false);
+            break;
+        case _LAYER2:
+            oled_write_ln_P(PSTR("Layer 2\n"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Undefined\n"), false);
+            break;
+    }
+
+    oled_write_P(PSTR("\n\n"), false);
+
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+}
+
+bool oled_task_kb(void) {
+    if (!oled_task_user()) {
+        return false;
+    }
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        render_logo();
+    }
+    return true;
+}
+#endif
+
+/**
  * @brief Mapping arrays for LED to matrix position conversion
  */
 int8_t led_to_matrix_row[RGB_MATRIX_LED_COUNT];
@@ -251,3 +311,4 @@ void keyboard_post_init_kb(void) {
     precalculate_all_led_colors();
     keyboard_post_init_user();
 }
+
